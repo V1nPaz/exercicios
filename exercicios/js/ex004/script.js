833f72c72b437f0c25232185c2d502715ddc38be
ViniciusPaz <vinicius.paz@cepedi.org.br>
2025-09-18 16:59:45 -0300

    exemplo com temporizadores

---
+++ exercicios/js/ex004/script.js
@@ -0,0 +1,7 @@
+function escrever(){
+    document.writeln("<p>Escrevendo...</p>")
+}
+
+//setInterval(escrever, 1000);              //escreve periodicamente
+
+setTimeout(escrever, 10000);                //escreve após um intervalo
