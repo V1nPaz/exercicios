dac487e17eae48b56d53263ec61b21cc4387b575
ViniciusPaz <vinicius.paz@cepedi.org.br>
2025-10-06 16:30:57 -0300

    Rotas de criar sessão e de cadastrar casa

--- node/house-man/src/controllers/SessionController.js
+++ node/house-man/src/controllers/SessionController.js
@@ -9,9 +9,13 @@ import User from "../models/User.js";
 class SessionControler{
     async store(req, res){
         const { email } = req.body;
-        let user = await User.create({email});
 
-        return res.json(email);
+        //Verifica 
+        let user = await User.findOne({email});
+        if (!user){
+            user = await User.create({email});
+        }
+        return res.json(user);
     }
 
 }
--- node/house-man/src/routes.js
+++ node/house-man/src/routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import SessionController from "./controllers/SessionController.js";
+import HouseController from "./controllers/HouseController.js";
+import multer from "multer";
+import uploadConfig from "./config/upload.js";
+
 const routes = new Router();
+const upload = multer(uploadConfig);
 
 routes.get("/", (req, res) => {
     return res.json({ok : true});
 });
 
 routes.post("/teste", SessionController.store);
+
+routes.post("/houses", upload.single("thumbnail"), HouseController.store);
 
 export default routes;
